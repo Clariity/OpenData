d93a274d361b39b4b934836672fa790bec982be9
rdg1g16 <rdg1g16@ecs.soton.ac.uk>
2020-03-06 22:31:26 +0000

    Updates

---
+++ precache-manifest.8a801ad749abe1b5aa4864f72ab4dbfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9275a44a95f70e62432b8a262cfcdbae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d53676d242ecd8cea498",
+    "url": "./static/css/main.269fa064.chunk.css"
+  },
+  {
+    "revision": "a16aaf63450228bb9923",
+    "url": "./static/js/2.26dfd931.chunk.js"
+  },
+  {
+    "revision": "c7420e3784398be5c0cd85ff7b468e6d",
+    "url": "./static/js/2.26dfd931.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d53676d242ecd8cea498",
+    "url": "./static/js/main.70b2da49.chunk.js"
+  },
+  {
+    "revision": "4ffe29df744ed789a6ef",
+    "url": "./static/js/runtime-main.91f9bf6a.js"
+  }
+]);
